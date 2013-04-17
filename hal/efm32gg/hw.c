7b0e694e5bc181fd1fa744885e3596de3624b5ca
Grissiom <chaos.proton@gmail.com>
2013-04-17 22:18:53 +0800

    hal/efm32gg: added

---
+++ hal/efm32gg/hw.c
@@ -0,0 +1,40 @@
+#include <em_msc.h>
+
+#include <fvs.h>
+
+rt_err_t fvs_begin_write(struct fvs_page *p)
+{
+	MSC_Init();
+	return RT_EOK;
+}
+
+rt_err_t fvs_native_write(void *addr, fvs_native_t data)
+{
+	MSC_Init();
+	msc_Return_TypeDef res;
+	fvs_debug("FVS: write %X to 0x%p\n", data, addr);
+	res = MSC_WriteWord(addr, &data, sizeof(data));
+	if (res != mscReturnOk)
+		return -RT_ERROR;
+
+	if (*(fvs_native_t*)addr != data)
+	{
+		rt_kprintf("FVS: write %X while got %X\n", data, *(fvs_native_t*)addr);
+		RT_ASSERT(0);
+	}
+
+	return RT_EOK;
+}
+
+rt_err_t fvs_end_write(struct fvs_page *p)
+{
+	MSC_Deinit();
+	return RT_EOK;
+}
+
+rt_err_t fvs_erase_page(struct fvs_page *page)
+{
+	MSC_ErasePage(page->base_addr);
+	return RT_EOK;
+}
+
